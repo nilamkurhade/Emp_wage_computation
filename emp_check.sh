f62eb05ea46f92e44392e07743eddf5d1766cbfe
nilamkurhade <nilamkurhade87@gmail.com>
2021-04-05 10:31:28 +0530

    add: UC2 calculating daily emp wage

--- emp_check.sh
+++ emp_check.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x
 isPresent=1
 randomCheck=$((RANDOM%2))
+totalWorkingHrs=20
+empHrs=8
 if [ $isPresent -eq $randomCheck ]
 then
 	echo "emp present"
+	salary=$((totalWorkingHrs*empHrs))
 else
 	echo "emp not present"
+	salary=o
 fi
